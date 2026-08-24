3a1b75b39ac2d2c5bf092e3725a2325638685665
eZin0r <alexsandro@gomesalves.com.br>
2026-08-24 18:09:59 -0300

    Set compileSdk 36 for AndroidX Activity 1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.com.heydjow.yolotestometro"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "br.com.heydjow.yolotestometro"
